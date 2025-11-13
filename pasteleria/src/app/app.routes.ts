97ea85af777ee320a3e99a74e9f9c841d5cd84f1
cristinaKris <isidrocastrokarencristina@gmail.com>
2025-11-13 11:33:42 -0600

    Creaci

--- pasteleria/src/app/app.routes.ts
+++ pasteleria/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { ClienteComponent } from './pages/cliente/cliente.component';
+import { CarritoComponent } from './pages/carrito/carrito.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     { path: '', redirectTo: '/home', pathMatch: 'full' },
+    {path: 'home', component: HomeComponent},
+    {path: 'about-us', component: AboutUsComponent},
+    {path: 'catalogo', component: CatalogoComponent},
+    {path: 'cliente', component: ClienteComponent},
+    {path: 'carrito', component: CarritoComponent},
+    {path: '**', redirectTo: '/home'}
+];
