f0a369cd521725ca9b0f3fc4b2cb88bb4a0fda22
Carolina Figueroa Ruiz <carolina.figue13@gmail.com>
2025-11-20 14:43:40 -0600

    PruebaCaro

--- pasteleria/src/app/pages/home/home.component.ts
+++ pasteleria/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+}*/
+
+import { Component } from '@angular/core';
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ProductCardComponent, CommonModule, RouterModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+  productosConDescuento = [
+    { name: "Croissant", description: "Delicioso croissant relleno de crema", price: 5, img: "assets/img/croissant.jpg" },
+    { name: "Concha", description: "Suave pan dulce con un toque de chocolate", price: 5, img: "assets/img/concha.jpg" },
+    { name: "Trenza", description: "Trenza de pan con azúcar", price: 5, img: "assets/img/trenza.jpg" }
+  ];
+
 }
+
