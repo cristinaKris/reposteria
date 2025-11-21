b50715f3cf3c0c03681719f5844b8e8d1542e880
Carolina Figueroa Ruiz <carolina.figue13@gmail.com>
2025-11-20 22:28:51 -0600

    AboutUsCaro

--- pasteleria/src/app/pages/home/home.component.ts
+++ pasteleria/src/app/pages/home/home.component.ts
@@ -17,9 +17,9 @@ import { CarrouselComponent } from "../../components/carrousel/carrousel.compone
 export class HomeComponent {
   constructor (private dialog:MatDialog){};
   productosConDescuento: {name: string; description: string; price: number; img:string}[] = [
-    { name: "Croissant", description: "Delicioso croissant que muchos llaman la especialidad de la casa.", price: 15, img: "assets/img/croissant.png" },
-    { name: "Concha", description: "El clásico pan dulce suave con una costra de azucar encima.", price: 15, img: "assets/img/concha.png" },
-    { name: "Trenza", description: "Trenza de pan con azúcar sin relleno.", price: 15, img: "assets/img/trenza.png" },
+    { name: "Croissant", description: "Delicioso croissant que muchos llaman la especialidad de la casa.", price: 10, img: "assets/img/croissant.png" },
+    { name: "Concha", description: "El clásico pan dulce suave con una costra de azucar encima.", price: 10, img: "assets/img/concha.png" },
+    { name: "Trenza", description: "Trenza de pan con azúcar sin relleno.", price: 10, img: "assets/img/trenza.png" },
     { name: "Chocolate Fresas", description: "Pastel húmedo de chocolate cubierto con fresas frescas y ganache intenso.", price: 450, img: "assets/img/pasteles/chocolateFresas.png" },
     { name: "Chocolate Pistache", description: "Pastel de chocolate oscuro con capas de crema de pistache y decoración elegante.", price: 480, img: "assets/img/pasteles/chocolatePistache.png" },
     { name: "Doble Chocolate", description: "Pastel doble capa con chocolate intenso y cobertura espesa estilo gourmet.", price: 470, img: "assets/img/pasteles/dobleChocolate.png" }
