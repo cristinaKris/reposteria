3653b4697d032cfcceee538fd13ccc4f3479d363
Carolina Figueroa Ruiz <carolina.figue13@gmail.com>
2025-11-20 20:40:30 -0600

    PáginaHomeCaro

--- pasteleria/src/app/pages/home/home.component.ts
+++ pasteleria/src/app/pages/home/home.component.ts
@@ -1,33 +1,28 @@
-/*import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
-})
-export class HomeComponent {
-
-}*/
 
 import { Component } from '@angular/core';
 import { ProductCardComponent } from '../../components/product-card/product-card.component';
 import { CommonModule } from '@angular/common';
+import {Router} from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { CarrouselComponent } from "../../components/carrousel/carrousel.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductCardComponent, CommonModule, RouterModule],
+  imports: [ProductCardComponent, CommonModule, RouterModule, CarrouselComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
-  productosConDescuento = [
-    { name: "Croissant", description: "Delicioso croissant relleno de crema", price: 5, img: "assets/img/croissant.jpg" },
-    { name: "Concha", description: "Suave pan dulce con un toque de chocolate", price: 5, img: "assets/img/concha.jpg" },
-    { name: "Trenza", description: "Trenza de pan con azúcar", price: 5, img: "assets/img/trenza.jpg" }
+  constructor (private dialog:MatDialog){};
+  productosConDescuento: {name: string; description: string; price: number; img:string}[] = [
+    { name: "Croissant", description: "Delicioso croissant que muchos llaman la especialidad de la casa.", price: 15, img: "assets/img/croissant.png" },
+    { name: "Concha", description: "El clásico pan dulce suave con una costra de azucar encima.", price: 15, img: "assets/img/concha.png" },
+    { name: "Trenza", description: "Trenza de pan con azúcar sin relleno.", price: 15, img: "assets/img/trenza.png" },
+    { name: "Chocolate Fresas", description: "Pastel húmedo de chocolate cubierto con fresas frescas y ganache intenso.", price: 450, img: "assets/img/pasteles/chocolateFresas.png" },
+    { name: "Chocolate Pistache", description: "Pastel de chocolate oscuro con capas de crema de pistache y decoración elegante.", price: 480, img: "assets/img/pasteles/chocolatePistache.png" },
+    { name: "Doble Chocolate", description: "Pastel doble capa con chocolate intenso y cobertura espesa estilo gourmet.", price: 470, img: "assets/img/pasteles/dobleChocolate.png" }
   ];
 
 }
