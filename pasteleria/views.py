b393235f081b3b8b79da3c1e76024420fbddf299
cristinaKris <isidrocastrokarencristina@gmail.com>
2025-11-23 12:30:14 -0600

    archivosEndPoints

--- pasteleria/views.py
+++ pasteleria/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+import json
 
-# Create your views here.
