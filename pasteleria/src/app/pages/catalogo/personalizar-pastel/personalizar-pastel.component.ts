4e131f161c28a033a0655b323e74e1de5e07f696
Axel Joshua <axellogran@gmail.com>
2025-11-24 00:24:15 -0600

    Conexion Django/Postgres

---
+++ pasteleria/src/app/pages/catalogo/personalizar-pastel/personalizar-pastel.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { AsistentePasosComponent } from '../../../components/asistente-pasos/asistente-pasos.component';
+import { timeout } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { MatDialog,MatDialogRef } from '@angular/material/dialog';
+import { DialogRef } from '@angular/cdk/dialog';
+import { Pastel } from '../../../models/pastel.model';
+
+@Component({
+  selector: 'app-personalizar-pastel',
+  imports: [AsistentePasosComponent, CommonModule],
+  templateUrl: './personalizar-pastel.component.html',
+  styleUrl: './personalizar-pastel.component.scss'
+})
+export class PersonalizarPastelComponent {
+  constructor(private dialogRef: MatDialogRef<PersonalizarPastelComponent>){}
+  pastel = new Pastel(
+    '',        // size
+    '',         // relleno
+    '',     // sabor
+    ''         // decoracion
+  );
+  pasos =[
+    {valor: 1, titulo:"Tamaño"},
+    {valor: 2, titulo:"Sabor"},
+    {valor: 3, titulo:"Relleno"},
+    {valor: 4, titulo:"Decoración"},
+    {valor: 5, titulo: "Resumen"}
+  ];
+  sizes=["Pequeño", "Mediano", "Grande"];
+  sabores=["Chocolate", "Matcha", "Zanahoria"];
+  rellenos=["Crema de cacahuate", "Durazno", "Ganache de chocolate"];
+  decoraciones=["Figuras de Fondant", "Mirror Glaze", "Flore snarurales comestibles"];
+  asistente=false;
+
+  imagen:string = "assets/img/decoracion3.png";
+
+  pasoActual=1;
+  cambiarPaso(p: number) {
+    this.pasoActual = p;
+  }
+  onClose(){
+    this.pasoActual = 1; 
+    this.pastel = undefined as any; 
+    this.dialogRef.close();
+  }
+
+  size(size:string){
+    this.pastel.size=size;
+    this.cambiarPaso(2);
+  }
+  
+  sabor(sabor:string){
+    this.pastel.sabor = sabor;
+    this.cambiarPaso(3);
+  }
+
+  relleno(relleno:string){
+    this.pastel.relleno=relleno;
+    this.cambiarPaso(4);
+  }
+
+  decoracion(deco:string){
+    this.pastel.decoracion=deco;
+    this.cambiarPaso(5);
+  }
+
+  finalizar(){
+    alert('¡Pedido confirmado!');
+
+    this.dialogRef.close();
+  }
+
+
+}
